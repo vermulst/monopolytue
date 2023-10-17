ae3cd4cffeb9986cf23e4da3a4cf64582f4ba579
TimovanDoorn <timovandoorn24@gmail.com>
2023-10-17 16:08:49 +0200

    Images uploaded for the dices.

--- me/tue/monopolytue/Chancecard.java
+++ me/tue/monopolytue/Chancecard.java
@@ -6,12 +6,16 @@ public class Chancecard {
     Random random = new Random(0);
 
     String[] stringCards = new String[5]; 
+    int[] balanceChange = {-100, -50, 50, 100, 150};
 
     public Chancecard() {
         stringCards[0] = "You have been convicted of money laundering. Pay a fine of EUR 100.";
         stringCards[1] = "You have exceeded the speed limit. Pay a fine of EUR 50.";
         stringCards[2] = "It's your birthday. You receive a gift of EUR 50.";
-        stringCards[3] = "Your stocks have increased. You receive a gig";
+        stringCards[3] = "Your stocks have increased. You receive 100 EUR in dividens.";
+        stringCards[4] = "You won a lawsuit. You earn 150 EUR";
+
+
     }
 
     void pullChancecard() {
