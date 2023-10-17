5a14e82b7b3dc37fdb57fcd2f43ef008c5558647
TimovanDoorn <timovandoorn24@gmail.com>
2023-10-17 17:11:23 +0200

    Modified the Participant class, to adjust the balances of the players and whether a player is bankrupt or not.

--- me/tue/monopolytue/turn/Participant.java
+++ me/tue/monopolytue/turn/Participant.java
@@ -1,5 +1,32 @@
 package me.tue.monopolytue.turn;
 
 public class Participant {
+
+    private int balance;
+    private boolean bankrupt = false;
+
+    public Participant() {
+
+    }
+
+    public void addToBalance(int amount) {
+        this.balance += amount;
+    }
+
+    public void removeToBalance(int amount) {
+        this.balance -= amount;
+        this.checkBankrupt();
+    }
+
+    public void transferAmount(Participant receiver, int amount) {
+        receiver.addToBalance(amount);
+        this.removeToBalance(amount);
+    }
+
+    public void checkBankrupt() {
+        if (this.balance < 0) {
+            this.bankrupt = true;
+        }
+    }
     
 }
